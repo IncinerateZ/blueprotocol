0b632800eb880b082fede2619fa9e47edd5a74df
IncinerateZ <inferious77@gmail.com>
2023-04-19 15:56:29 +0900

    refactor langpicker & mappicker

--- components/Maps/MapControlLayer.js
+++ components/Maps/MapControlLayer.js
@@ -2,14 +2,13 @@
 import Image from 'next/image';
 
 import ChevronLeft from '../../public/map/chevron-left.svg';
-import ChevronLeftDark from '../../public/map/chevron-left-black.svg';
-import ChevronLight from '../../public/map/chevron-left-light.svg';
 import ChevronLightWhite from '../../public/map/chevron-left-light-white.svg';
 
-import { levenshtein } from '../utils';
 import { useRef, useState } from 'react';
 
 import styles from '../../styles/Map.module.css';
+import LangPicker from './LangPicker';
+import MapPicker from './MapPicker';
 
 export default function MapControlLayer({
     data,
@@ -37,68 +36,7 @@ export default function MapControlLayer({
     setSelectorsSource,
 }) {
     const [chevron, setChevron] = useState(true);
-    const [doLangDrop, setDoLangDrop] = useState(false);
     const [collapsedSelectors, setCollapsedSelectors] = useState({});
-
-    const searchRef = useRef();
-
-    const ReadableString = { en_US: 'EN', ja_JP: 'JP' };
-
-    function handleMapSearch(e) {
-        let q = e?.target?.value;
-        if (q?.length >= 0) setMapSearch(q);
-        q = q?.toLowerCase() || '';
-
-        let res = {};
-
-        for (let k of Object.keys(maps)) {
-            let ks = k.toLowerCase().indexOf(q);
-            if (ks != -1) {
-                for (let t of maps[k]) {
-                    res[
-                        lang === 'ja_JP'
-                            ? DB.LocationNames[lang][data[t].map_id]
-                            : data[t].display_name
-                    ] = ks;
-                }
-            } else {
-                let d = levenshtein(k.toLowerCase(), q);
-                if (d <= 5)
-                    for (let t of maps[k]) {
-                        let name =
-                            lang === 'ja_JP'
-                                ? DB.LocationNames[lang][data[t].map_id]
-                                : data[t].display_name;
-                        if (res[name] >= 0) continue;
-                        res[name] = 100 + d;
-                    }
-            }
-        }
-
-        let res_ = Object.keys(res);
-        res_.sort((a, b) => {
-            return res[a] - res[b];
-        });
-
-        setSSHighlight(0);
-        setSearchSuggestions(res_);
-    }
-
-    function handleSuggestionsSelect(code) {
-        if (searchSuggestions.length === 0) return;
-        if (code === 'ArrowUp') setSSHighlight((e) => Math.max(0, e - 1));
-
-        if (code === 'ArrowDown')
-            setSSHighlight((e) =>
-                Math.min(searchSuggestions.length - 1, e + 1),
-            );
-        if (code === 'Enter') {
-            searchRef.current.blur();
-            let c = searchSuggestions[ssHighlight];
-            setChosenMap(maps[c][0]);
-            resetSearch();
-        }
-    }
 
     return (
         <div
@@ -107,115 +45,24 @@ export default function MapControlLayer({
             }`}
         >
             <div className={styles.MCL_content}>
-                <div className={`${styles.langPicker} ${styles.noSelect}`}>
-                    <div
-                        className={styles.langPicker_display}
-                        style={{
-                            borderBottomLeftRadius: doLangDrop ? '0' : '5px',
-                        }}
-                        onClick={() => {
-                            setDoLangDrop((p) => !p);
-                        }}
-                    >
-                        {ReadableString[lang]}
-                    </div>
-                    <div style={{ position: 'relative' }}>
-                        <Image
-                            src={ChevronLeftDark.src}
-                            width={25}
-                            height={25}
-                            alt={'Change Maps'}
-                            style={{
-                                position: 'absolute',
-                                transform:
-                                    'translate(140%, -110%) ' +
-                                    `rotate(${doLangDrop ? '-270' : '-90'}deg)`,
-                                pointerEvents: 'none',
-                                transition: '0.1s',
-                            }}
-                        ></Image>
-                    </div>
-                    {doLangDrop && (
-                        <div className={styles.langPicker_dropdown}>
-                            {Object.keys(DB.Loc)
-                                .filter((lang_) => lang !== lang_)
-                                .map((lang_) => (
-                                    <div
-                                        key={Math.random()}
-                                        onClick={() => {
-                                            setDoLangDrop(false);
-                                            setLang(lang_);
-                                        }}
-                                    >
-                                        {ReadableString[lang_]}
-                                    </div>
-                                ))}
-                        </div>
-                    )}
-                </div>
-                <span style={{ marginTop: '4px' }}>
-                    <b>Map</b>
-                </span>
-                <div className={styles.MCL_mapsearch}>
-                    <input
-                        type={'text'}
-                        value={
-                            doSearch || !DB || lang !== 'ja_JP'
-                                ? mapSearch
-                                : DB?.LocationNames[lang]?.[
-                                      data[chosenMap].map_id
-                                  ]
-                        }
-                        onChange={(e) => {
-                            handleMapSearch(e);
-                            setDoSearch(true);
-                        }}
-                        onKeyDown={(e) => handleSuggestionsSelect(e.code)}
-                        onClick={() => {
-                            if (!doSearch) {
-                                handleMapSearch();
-                                setDoSearch(true);
-                            } else resetSearch();
-                        }}
-                        ref={searchRef}
-                    ></input>
-                    <div style={{ position: 'relative' }}>
-                        <Image
-                            src={ChevronLight.src}
-                            width={25}
-                            height={25}
-                            alt={'Change Maps'}
-                            style={{
-                                position: 'absolute',
-                                transform:
-                                    'translate(285px, -120%) ' +
-                                    `rotate(${doSearch ? '-270' : '-90'}deg)`,
-                                pointerEvents: 'none',
-                                transition: '0.1s',
-                            }}
-                        ></Image>
-                    </div>
-                    <div className={styles.mapsearch_suggestions}>
-                        {doSearch &&
-                            searchSuggestions.map((e, i) => (
-                                <div
-                                    key={Math.random()}
-                                    className={
-                                        ssHighlight === i
-                                            ? styles.highlight
-                                            : ''
-                                    }
-                                    onClick={() => {
-                                        searchRef.current.blur();
-                                        setChosenMap(maps[e][0]);
-                                        resetSearch();
-                                    }}
-                                >
-                                    {e}
-                                </div>
-                            ))}
-                    </div>
-                </div>
+                <LangPicker DB={DB} lang={lang} setLang={setLang} />
+                <MapPicker
+                    DB={DB}
+                    data={data}
+                    doSearch={doSearch}
+                    mapSearch={mapSearch}
+                    setMapSearch={setMapSearch}
+                    ssHighlight={ssHighlight}
+                    setSSHighlight={setSSHighlight}
+                    searchSuggestions={searchSuggestions}
+                    setSearchSuggestions={setSearchSuggestions}
+                    chosenMap={chosenMap}
+                    setChosenMap={setChosenMap}
+                    resetSearch={resetSearch}
+                    lang={lang}
+                    maps={maps}
+                    setDoSearch={setDoSearch}
+                />
                 <div
                     style={{ marginTop: '16px', height: '100%' }}
                     className={styles.MCL_selectorsContainer}
