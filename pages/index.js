925564559f448aabbbce20b284882d09627638e1
IncinerateZ <inferious77@gmail.com>
2023-05-16 12:52:03 +0900

    clone map page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
 import Head from 'next/head';
 
-import styles from '@/styles/Home.module.css';
-import Map from './map';
-
 export default function Home() {
+    const Map = dynamic(() => import('@/components/Maps/AsterleedsMap'), {
+        loading: () => (
+            <div
+                style={{
+                    width: '100vw',
+                    height: '100vh',
+                    display: 'flex',
+                    position: 'absolute',
+                    backgroundColor: 'black',
+                    zIndex: '99999',
+                    opacity: '0.8',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'white',
+                }}
+            >
+                <span>Loading...</span>
+            </div>
+        ),
+        ssr: false,
+    });
     return (
-        <div className={styles.bg}>
+        <div>
             <Head>
-                <title>Blue Protocol Resource</title>
-                <meta name='description' content='Blue Protocol Resource' />
-                <link rel='icon' href='/favicon.ico' />
+                <title>
+                    Blue Protocol Interactive Map | Blue Protocol Resource
+                </title>
             </Head>
-
             <Map></Map>
         </div>
     );
