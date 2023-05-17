5a4ffee5fa6e6b1af5f6503ec91745d98b42f88f
IncinerateZ <inferious77@gmail.com>
2023-05-18 00:47:24 +0900

    canonical

--- pages/contact.js
+++ pages/contact.js
@@ -1,17 +1,26 @@
+import Head from 'next/head';
+
 export default function contact() {
     return (
-        <div style={{ margin: '2rem' }}>
-            <h1>ONLY IMPORTANT AND RELEVANT MATTERS WILL BE RESPONDED TO</h1>
-            <p>Discord: IncinerateZ#4038</p>
-            <p>Email: admin@incin.net</p>
-            <div
-                style={{ cursor: 'pointer', color: 'blue' }}
-                onClick={() => {
-                    window.location = './';
-                }}
-            >
-                Back to Homepage
+        <>
+            <Head>
+                <link rel='canonical' href='https://bp.incin.net/contact' />
+            </Head>
+            <div style={{ margin: '2rem' }}>
+                <h1>
+                    ONLY IMPORTANT AND RELEVANT MATTERS WILL BE RESPONDED TO
+                </h1>
+                <p>Discord: IncinerateZ#4038</p>
+                <p>Email: admin@incin.net</p>
+                <div
+                    style={{ cursor: 'pointer', color: 'blue' }}
+                    onClick={() => {
+                        window.location = './';
+                    }}
+                >
+                    Back to Homepage
+                </div>
             </div>
-        </div>
+        </>
     );
 }
