332f6d0d251c6442033647fd9b8f763aea69c000
IncinerateZ <inferious77@gmail.com>
2023-08-13 21:56:13 +0900

    tiny optimization?

--- components/Maps/AsterleedsMap.js
+++ components/Maps/AsterleedsMap.js
@@ -505,7 +505,7 @@ export default function Map() {
                                   .replace('dng', 'pub')
                                   .replace('pat', 'pub')
                           ] || 'Loading'}{' '}
-                    Map | Blue Protocol Resource
+                    Interactive Map | Blue Protocol Resource
                 </title>
                 <link rel='canonical' href='https://bp.incin.net/map' />
                 <meta
@@ -653,15 +653,16 @@ export default function Map() {
                         {mapIcons &&
                             Object.keys(markers).map((e) =>
                                 markers[e].arr.map((v, i) => {
-                                    return (
-                                        <div key={i}>
-                                            {(v.selectors || []).reduce(
-                                                (s, c) =>
-                                                    s +
-                                                    (excludedSelectors[c] || 0),
-                                                0,
-                                            ) !== (v.selectors || []).length &&
-                                                mapIcons[v.type] && (
+                                    let showMarker =
+                                        (v.selectors || []).reduce(
+                                            (s, c) =>
+                                                s + (excludedSelectors[c] || 0),
+                                            0,
+                                        ) !== (v.selectors || []).length;
+                                    if (showMarker)
+                                        return (
+                                            <div key={i}>
+                                                {mapIcons[v.type] && (
                                                     <Marker
                                                         position={[
                                                             v.lat,
@@ -707,8 +708,8 @@ export default function Map() {
                                                         </Popup>
                                                     </Marker>
                                                 )}
-                                        </div>
-                                    );
+                                            </div>
+                                        );
                                 }),
                             )}
                         {/* <ClickHandler /> */}
